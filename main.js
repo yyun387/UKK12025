27eec4d70b50a9e6552068b60d3533d09d4c56a2
Yuyun yuningsih <yyyuyun818@gmail.com>
2025-02-06 13:12:30 +0700

    Update main.js

--- main.js
+++ main.js
@@ -8,9 +8,13 @@ calculate.addEventListener('click', (e) => {
     let percentage = document.getElementById('discount-percentage').value;
     let discountAmt = document.getElementById('discount-amount');
     let FinalPay = document.getElementById('pay');
+    let harga = document.getElementById('harga');
+    let total = document.getElementById('total');
 
     discountAmt.value = billAmt * percentage / 100;
     FinalPay.value = billAmt - discountAmt.value;
+    harga.value = billAmt * percentage / 100;
+    total.value = billAmt - discountAmt.value;
 });
 
 resetBtn.addEventListener('click', () => {
